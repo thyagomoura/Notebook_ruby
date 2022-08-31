02e6dcc6e71384d9e09e88c7551250ad7366280a
thyagomoura <thyagohenriquemoura@gmail.com>
2022-08-31 10:45:45 -0300

    APP: CRUD completo e funcional, com metodos para criacao, atualizacao, leitura e delecao

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact.to_br
+    render json: @contact, include: [:kind, :phones]
   end
 
   # POST /contacts
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      render json: @contact, status: :created, location: @contact
+      render json: @contact, include: [:kind, :phones], status: :created, location: @contact
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,12 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:name, :email, :birthdate, :kind_id)
+      params.require(:contact).permit(
+        :name, 
+        :email, 
+        :birthdate, 
+        :kind_id, 
+        phones_attributes: [:id, :number, :_destroy]
+      )
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,17 @@
 class Contact < ApplicationRecord
     #associacoes
-    belongs_to :kind, optional: true
+    belongs_to :kind
     has_many :phones
 
+    #nested 
+    accepts_nested_attributes_for :phones, allow_destroy: true
+
+    def as_json(options = {})
+        h = super(options)
+        h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+        h
+    end
+    #################testes e estudos######################
     # def author
     #     "Thyagomoura"
     # end
@@ -27,11 +36,12 @@ class Contact < ApplicationRecord
     #     I18n.l(self.birthdate) unless self.birthdate.blank?
     # end
  #ok
-    def to_br
-        {
-            name: self.name,  
-            email: self.email,
-            birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-        }
-    end
+    # def to_br
+    #     {
+    #         name: self.name,  
+    #         email: self.email,
+    #         birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    #         kind: {only: :description}
+    #     }
+    # end
 end
