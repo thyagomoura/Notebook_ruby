19d0c4516539d29ffeb8b895e3832a64e49a27e9
thyagomoura <thyagohenriquemoura@gmail.com>
2022-09-07 14:34:37 -0300

    CONTROLLER: Atualizando a controler de phones e criando o crud completo dessa controller

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,15 +1,48 @@
 class PhonesController < ApplicationController
 
-    before_action :set_phones
+    before_action :set_contact
 
-    # GET /phones/1
+    # GET contacts/1/phones/
     def show
-        render json: @phones
+        render json: @contact.phones
+    end
+
+    # POST contacts/1/phone/
+    def create
+        @contact.phones = Phone.new(phone_params)
+
+        if @contact.save
+            render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+        end
+    end
+
+    # PUT contacts/1/phone/
+    def update
+        phone = Phone.find(params[:id])
+
+        if phone.update(phone_params)
+            render json: @contact.phones
+        else
+            render json: @contact.errors, status: :unprocessable_entity
+        end
+    end
+
+
+     # DELETE contacts/1/phones/
+    def destroy
+        phone = Phone.find(params[:id])
+        phones.destroy
     end
 
     private
     # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-        @phones = Contact.find(params[:contact_id]).phones
+    def set_contact
+        @contact = Contact.find(params[:contact_id])
+    end
+
+    def address_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
--- app/serializers/phone_serializer.rb
+++ app/serializers/phone_serializer.rb
@@ -1,4 +1,8 @@
 class PhoneSerializer < ActiveModel::Serializer
-    attributes :id, :number
+  attributes :id, :number
+
+  belongs_to :contact do
+      link(:related) {contact_phones_url(object.contact.id)}
+  end
 end
   #aqui passamos os valores do model para retornarem no json da require
