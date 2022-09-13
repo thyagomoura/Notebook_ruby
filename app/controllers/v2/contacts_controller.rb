246daa9ed8a49be43d7837fab9936fc662a6dc7f
thyagomoura <thyagohenriquemoura@gmail.com>
2022-09-13 00:03:03 -0300

    APP: Adicionado versionamento para começar a trabalhar com o fluxo de informacao em 2 versoes, implementado v1 e v2 com diferenca apenas em contacts

---
+++ app/controllers/v2/contacts_controller.rb
@@ -0,0 +1,62 @@
+module V2
+  class ContactsController < ApplicationController
+    before_action :set_contact, only: %i[ show update destroy ]
+
+    # GET /contacts
+    def index
+      @contacts = Contact.last(5)
+      render json: @contacts# methods: [:i18n, :birthdate_br]
+    end
+
+    # GET /contacts/1
+    def show
+      render json: @contact, include: [:kind, :phones, :address]
+    end
+
+    # POST /contacts
+    def create
+      @contact = Contact.new(contact_params)
+
+      if @contact.save
+        render json: @contact, include: [:kind, :phones, :address], status: :created, location: @contact
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /contacts/1
+    def update
+      if @contact.update(contact_params)
+        render json: @contact, include: [:kind, :phones, :address]
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /contacts/1
+    def destroy
+      @contact.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_contact
+        @contact = Contact.find(params[:id])
+      end
+
+      #parametros que sao passados no json
+      # Only allow a list of trusted parameters through.
+      def contact_params
+        # params.require(:contact).permit(
+        #   :name, 
+        #   :email, 
+        #   :birthdate, 
+        #   :kind_id, 
+        #   phones_attributes: [:id, :number, :_destroy],
+        #   address_attributes: [:id, :street, :city]
+        # )
+        #conversao do json em algo que o ruby interprete
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,37 @@ Rails.application.routes.draw do
   resources :auths, only: [:create]
   resources :kinds
 
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request|request.params[:version] == "1" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
 
-    resource :phone, only: [:update, :create, :destroy]
-    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
 
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request|request.params[:version] == "2" } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
   end
 end
